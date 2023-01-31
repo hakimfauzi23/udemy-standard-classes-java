e674a203f28c25ceb776e9b674b5839dd8cef3b4
Hanif <hanif.hakim@spesolution.com>
2023-01-31 10:47:00 +0700

    Base64, Objects, and Random Classes

---
+++ src/hanif/hakim/classes/Base64App.java
@@ -0,0 +1,18 @@
+package hanif.hakim.classes;
+
+import java.util.Base64;
+
+public class Base64App {
+    public static void main(String[] args) {
+        String original = "Hanif Fauzi Hakim";
+
+
+        String encoded = Base64.getEncoder().encodeToString(original.getBytes());
+        System.out.println(encoded);
+
+        byte[] bytes = Base64.getDecoder().decode(encoded);
+        String result = new String(bytes);
+
+        System.out.println(result);
+    }
+}
