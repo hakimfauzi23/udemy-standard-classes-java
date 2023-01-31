4684e745fa7716cf38e84aea7b6472a0bcfb1f9c
Hanif <hanif.hakim@spesolution.com>
2023-01-31 10:23:15 +0700

    Learn Runtime, System, and UUID Classes

---
+++ src/hanif/hakim/classes/UUIDApp.java
@@ -0,0 +1,14 @@
+package hanif.hakim.classes;
+
+import java.util.UUID;
+
+public class UUIDApp {
+    public static void main(String[] args) {
+        for (var i=0;i<100;i++){
+            UUID uuid =UUID.randomUUID();
+            String key = uuid.toString();
+
+            System.out.println(key);
+        }
+    }
+}
